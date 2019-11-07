08de8b3c9c2374b3f71f944fbe9948912c59088d
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2019-11-07 16:30:18 +0200

    new file:   index.php new file:   style.css new file:   templates/footer.php new file:   templates/header.php

---
+++ templates/header.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ninja Pizza</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="main-screen">
+        <header>
+            <nav>
+                <ul class="navigation clearfix">
+                    <li class="navigation"><a class="navigation" href="https://www.w3schools.com/default.asp">HOME</a></li>
+                    <li class="navigation"><a class="navigation" href="https://www.w3schools.com/default.asp">NEWS</a></li>
+                    <li class="navigation"><a class="navigation" href="https://www.w3schools.com/default.asp">CONTACT</a></li>
+                    <li class="navigation"><a class="navigation" href="https://www.w3schools.com/default.asp">ABOUT</a></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
